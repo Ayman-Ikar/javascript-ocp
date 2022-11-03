c8ff0cddfbbc24aea7a33fd6c6799b3e98c712e0
Ayman Ikar <37595519+Ayman-Ikar@users.noreply.github.com>
2022-11-03 19:48:51 +0100

    Prototypes

--- demo.js
+++ demo.js
@@ -1,6 +1,53 @@
 'use strict';
 (function () {
 
+
+    // Create object literal manually
+    function Person(firstname, lastname, age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+
+        this.isAdult = function () {return this.age > 18;}
+
+        Object.defineProperty(this, 'fullName', {
+            get : function () {
+                return this.firstname + ' ' + this.lastname;
+            },
+            enumerable: true
+        })
+    }
+
+
+    function Student(firstname, lastname, age) {
+        Person.call(this, firstname, lastname, age);
+
+        this._enrolledCourses = [];
+        this.enroll = function(courseId) {
+            this._enrolledCourses.push(courseId);
+        }
+        this.getCourses = function () {
+            return this.fullName + " " + this._enrolledCourses.join(',');
+        }
+    }
+
+    let ayman = new Student('Ayman', 'Ikar', 26);
+
+    ayman.enroll(123);
+    ayman.enroll(564);
+
+    display(ayman.getCourses())
+
+})();
+
+
+
+
+
+
+
+function module1(){
+
     // Create object literal manually
     function Person(firstname, lastname, age, myFunction) {
         this.firstname = firstname;
@@ -43,4 +90,4 @@
     display({...person, ...healthStats});
     display(Object.assign({...person}, healthStats))
     display(person);
-})();
+}
